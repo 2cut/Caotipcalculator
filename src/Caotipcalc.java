aa32255d2bbf8b28d36d631ae475c102729c06d8
2cut <jialiangc@nycstudents.net>
2022-10-01 11:56:12 -0400

    Fixed rounding

--- src/Caotipcalc.java
+++ src/Caotipcalc.java
@@ -15,13 +15,14 @@ public class Caotipcalc {
         /* Calculating */
         double tip = bill*percent/100;
         double tipper = tip/people;
+        double roundtipper = Math.round(tipper*100.0);
         double billtip = bill + tip;
         double totalper = billtip/people;
 
         /* Results */
         System.out.println("Your total tip is " + tip);
         System.out.println("The total bill including tip is " + billtip);
-        System.out.println("The tip per person is " + tipper);
+        System.out.println("The tip per person is " + roundtipper/100.0);
         System.out.println("The total for each person is " + totalper);
     }
 }
