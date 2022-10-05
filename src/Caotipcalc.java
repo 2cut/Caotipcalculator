7678d2c2966648aefaf1d3e25b37baa51ee4bf28
2cut <jialiangc@nycstudents.net>
2022-10-04 22:08:46 -0400

    Done

--- src/Caotipcalc.java
+++ src/Caotipcalc.java
@@ -4,7 +4,7 @@ public class Caotipcalc {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        /* asking questions */
+        /* Asking questions to get values for bill amount, tip percent, and total people*/
         System.out.println("How much was the bill?");
         double bill = scan.nextDouble();
         System.out.println("What percent would you like to tip?");
@@ -12,14 +12,15 @@ public class Caotipcalc {
         System.out.println("How many people are there?");
         double people = scan.nextDouble();
 
-        /* Calculating */
+        /* Calculating Results*/
         double tip = bill*percent/100;
         double tipPer = tip/people;
+        // Using Math.round method after multiplying by 100 allows us to round to the hundredths place one divided back
         double roundTipper = Math.round(tipPer*100.0);
         double billTip = bill + tip;
         double totalPer = billTip/people;
 
-        /* Results */
+        /* Printing results */
         System.out.println("Your total tip is " + tip);
         System.out.println("The total bill including tip is " + billTip);
         System.out.println("The tip per person is " + roundTipper/100.0);
